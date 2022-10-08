f05e0b44127ba0a0540bb52b02cb0fb79678f30d
panvicka <panvicka@gmail.com>
2022-10-08 20:56:51 +0200

    trust me, no idea

--- src/components/itemDetail.svelte
+++ src/components/itemDetail.svelte
@@ -1,8 +1,52 @@
 <script>
+	import { SettingsIcon } from 'svelte-feather-icons';
+
 	export let item;
+
+	let name = item.name;
+
+	let result = null;
+
+	function formHandler(event) {
+		event.preventDefault();
+
+		fetch(`http://localhost:9090/rotationItems/update/${item._id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name,
+			})
+		})
+			.then((response) => response.json())
+			.then((result) => console.log(result));
+	}
+
 </script>
 
 <p>{item._id}</p>
 <p>{item.name}</p>
 <p>{item.memberTitles[0]}</p>
 <p>{item.memberTitles[1]}</p>
+
+<div class="collapse">
+	<input type="checkbox" />
+	<div class="collapse-title text-xl font-medium">
+		<SettingsIcon size="1.5x" />
+	</div>
+	<div class="collapse-content">
+		<div class="form-control w-full max-w-xs">
+			<label class="label">
+				<span class="label-text">Name</span>
+			</label>
+			<input
+				bind:value={name}
+				type="text"
+				placeholder="Type here"
+				class="input input-bordered w-full max-w-xs"
+			/>
+			<button class="btn btn-accent" on:click={formHandler}>Save</button>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="typescript">
 	import { items } from '../store/itemsStore';
 	import ItemCard from '../components/itemCard.svelte';
+	import { PlusCircleIcon } from 'svelte-feather-icons';
 
-	const handleOnClick = async () => console.log('click');
+	let name = '';
+	let members = '';
 </script>
 
 <svelte:head>
@@ -10,10 +12,40 @@
 </svelte:head>
 
 <div class="wrapper">
-	<button on:click={handleOnClick}>Get</button>
 	{#each $items as item}
 		<ItemCard {item} />
 	{/each}
+</div>
+
+<div class="collapse">
+	<input type="checkbox" />
+	<div class="collapse-title text-xl font-medium">
+		<PlusCircleIcon />
+	</div>
+	<div class="collapse-content">
+		<div class="form-control w-full max-w-xs">
+			<label class="label">
+				<span class="label-text">Name</span>
+			</label>
+			<input
+				bind:value={name}
+				type="text"
+				placeholder="Type here"
+				class="input input-bordered w-full max-w-xs"
+			/>
+
+			<label class="label">
+				<span class="label-text">Members</span>
+			</label>
+			<input
+				bind:value={members}
+				type="text"
+				placeholder="Type here"
+				class="input input-bordered w-full max-w-xs"
+			/>
+			<button class="btn btn-accent" on:click={formHandler}>Add item</button>
+		</div>
+	</div>
 </div>
 
 <style>

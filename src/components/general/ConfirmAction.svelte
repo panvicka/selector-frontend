d0921d0f5c7d4f71f49bcbe313cc9e5d54a27046
Eliska Veisova <eliska.veisova@bitgrip.de>
2022-10-20 19:13:28 +0200

    confirm action popup

---
+++ src/components/general/ConfirmAction.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function cancel() {
+		dispatch('cancel');
+	}
+
+	function ok() {
+		dispatch('ok');
+	}
+</script>
+
+<div>
+	<div>
+		<h1><slot name="title" /></h1>
+		<slot name="content" />
+	</div>
+	<button
+		class="btn btn-outline btn-error"
+		type="button"
+		on:click={() => {
+			cancel();
+		}}>Cancel</button
+	>
+	<button type="button" class="btn btn-outline btn-info" on:click={ok}>Ok</button>
+</div>
--- src/components/personCard.svelte
+++ src/components/personCard.svelte
@@ -2,21 +2,45 @@
 	import { DeleteIcon } from 'svelte-feather-icons';
 	import { deletePerson } from '../api/people';
 	import { createEventDispatcher } from 'svelte';
+	import Modal from './general/Modal.svelte';
+	import ConfirmAction from './general/ConfirmAction.svelte';
 
 	export let person;
 
 	const dispatch = createEventDispatcher();
 
-	function onDelete() {
-		dispatch('onDelete', {
-			personId: person._id
-		});
-	}
+	let deleteTriggered = false;
 </script>
+
+{#if deleteTriggered}
+	<Modal>
+		<ConfirmAction
+			on:cancel={() => {
+				deleteTriggered = false;
+			}}
+			on:ok={() => {
+				dispatch('onDelete', {
+					personId: person._id
+				});
+				deleteTriggered = false;
+			}}
+		>
+			<svelte:fragment slot="title">Confirmation</svelte:fragment>
+			<span slot="content"
+				>Do you really want to delete {person.name}? You can not reverse this action.
+			</span>
+		</ConfirmAction>
+	</Modal>
+{/if}
 
 <div class="card w-96 bg-base-100 shadow-xl" />
 <div class="card-body">
 	<h2 class="card-title"><a href={`people/${person._id}`}>{person.name}</a></h2>
-	<span on:click={onDelete}><DeleteIcon /> </span>
+	<span
+		on:click={() => {
+			deleteTriggered = true;
+		}}
+		><DeleteIcon />
+	</span>
 </div>
 <div class="card w-96 bg-base-100 shadow-xl" />

b1b271c78044304953bd52cf47ed7a8d329ba305
Eliska Veisova <eliska.veisova@bitgrip.de>
2022-10-28 09:35:25 +0200

    theme switcher

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,3 +1,13 @@
+<script>
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+
+	onMount(() => {
+		themeChange(false);
+		// 👆 false parameter is required for svelte
+	});
+</script>
+
 <header class="header-area">
 	<div class="navbar fixed top-0 left-0 right-0 z-50 bg-neutral-focus shadow-lg border-none">
 		<div class="navbar-start">
@@ -26,6 +36,44 @@
 					<li><a class="btn btn-ghost normal-case text-xl" href="/about">About</a></li>
 				</ul>
 			</div>
+		</div>
+
+		<div class="navbar-end">
+			<select
+				class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+				data-choose-theme
+			>
+				<option disabled value=""> Pick a theme </option>
+				<option value="">default (dark)</option>
+				<option value="light">light</option>
+				<option value="retro">retro</option>
+				<option value="dracula">dracula</option>
+				<option value="cyberpunk">cyberpunk</option>
+				<option value="cupcake">cupcake</option>
+				<option value="bumblebee">bumblebee</option>
+				<option value="emerald">emerald</option>
+				<option value="corporate">corporate</option>
+				<option value="synthwave">synthwave</option>
+				<option value="valentine">valentine</option>
+				<option value="halloween">halloween</option>
+				<option value="garden">garden</option>
+				<option value="forest">forest</option>
+				<option value="aqua">aqua</option>
+				<option value="lofi">lofi</option>
+				<option value="pastel">pastel</option>
+				<option value="fantasy">fantasy</option>
+				<option value="wireframe">wireframe</option>
+				<option value="black">black</option>
+				<option value="luxury">luxury</option>
+				<option value="cmyk">cmyk</option>
+				<option value="autumn">autumn</option>
+				<option value="business">business</option>
+				<option value="acid">acid</option>
+				<option value="lemonade">lemonade</option>
+				<option value="night">night</option>
+				<option value="coffee">coffee</option>
+				<option value="winter">winter</option>
+			</select>
 		</div>
 	</div>
 </header>
